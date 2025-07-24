b0bcaff99373c3220bfdd5da06244899113f5219
Charlie <charlies.retro.computing@gmail.com>
2025-07-23 21:43:48 -0400

    add some stuff mainly the main +page.svelte

---
+++ src/lib/components/title.svelte
@@ -0,0 +1,7 @@
+<script>
+    let { title, siteTitle = "PowerPCFan's Website", separator = "|" } = $props();
+</script>
+
+<svelte:head>
+    <title>{title} {separator} {siteTitle}</title>
+</svelte:head>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+    let { children } = $props();
+
+    import Header from "$lib/components/header.svelte";
+    import Footer from "$lib/components/footer.svelte";
+</script>
+
+<Header />
+
+<main>
+    {@render children()}
+</main>
+
+<Footer />
+
+<style lang="scss">
+    main {
+        margin: 0;
+        padding: 0;
+        width: 100%;
+    }
+</style>
