57128136b6648569cde1c87309171d4292cb2290
Charlie <charlies.retro.computing@gmail.com>
2025-07-25 13:04:19 -0400

    Final commit for website refactor

---
+++ src/routes/retro-computing/common-issues/+layout.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+    let { children } = $props();
+</script>
+
+<div class="container">
+    <div class="container-2">
+        {@render children()}
+    </div>
+</div>  
+
+<style lang="scss">
+    @use '/static/scss/global.scss' as g;
+
+    .container {
+        height: 100%;
+        width: auto;
+        padding: 2rem;
+        background-image: url("/images/background-tiles/2.png");
+        background-repeat: repeat;
+
+        .container-2 {
+            margin-inline: auto;
+            width: 90%;
+            background-color: g.$light;
+            color: g.$dark;
+            padding: 0.5rem 2rem 0.5rem 2rem;
+            border-radius: 1rem;
+        }
+    }
+</style>
--- src/routes/retro-computing/common-issues/+page.svelte
+++ src/routes/retro-computing/common-issues/+page.svelte
@@ -4,61 +4,37 @@
 
 <Title title="Common Problems with Old Macs" />
     
-<div class="container">
-    <div class="container-2">
-        <h1>Common Problems with Old Apple Macs</h1>
+<h1>Common Problems with Old Apple Macs</h1>
 
-        <div id="links">
-            <a href="/retro-computing/common-issues/vinegar-syndrome">Vinegar Syndrome</a>
-            <br /><br />
-            <a href="/retro-computing/common-issues/exploding-batteries">Exploding Batteries</a>
-            <br /><br />
-            <a href="/retro-computing/common-issues/crt-problems">CRT Problems</a>
-            <br /><br />
-            <a href="/retro-computing/common-issues/rifa-caps">Exploding RIFA Filter Caps</a>
-            <br /><br />
-        </div>
-    </div>
+<div id="links">
+    <a href="/retro-computing/common-issues/vinegar-syndrome">Vinegar Syndrome</a>
+    <a href="/retro-computing/common-issues/exploding-batteries">Exploding Batteries</a>
+    <a href="/retro-computing/common-issues/crt-problems">CRT Problems</a>
+    <a href="/retro-computing/common-issues/rifa-caps">Exploding RIFA Filter Caps</a>
 </div>
 
 <style lang="scss">
     @use '/static/scss/global.scss' as g;
 
-    .container {
-        height: 100%;
-        width: auto;
-        padding: 2rem;
-        background-image: url("/images/background-tiles/2.png");
-        background-repeat: repeat;
-
-        .container-2 {
-            margin-inline: auto;
-            width: 90%;
-            background-color: g.$light;
-            color: g.$dark;
-            padding: 1rem;
-            border-radius: 1rem;
-        }
-    }
-
-    a {
-        color: g.$link;
-        transition: color 0.15s ease;
-    
-        &:hover {
-            color: darken(g.$link, 25);
-            cursor: pointer;
-        }
-    }
-
     h1 {
         text-align: center;
-        font-size: 60px;
+        font-size: 3rem;
     }
 
-    a,
     #links {
-        text-align: center;
-        font-size: 25px;
+        margin-bottom: 2rem;
+        a {
+            margin-top: 0.25rem;
+            text-align: center;
+            font-size: 25px;
+            display: block;
+            color: g.$link;
+            transition: color 0.15s ease;
+        
+            &:hover {
+                color: darken(g.$link, 25);
+                cursor: pointer;
+            }
+        }
     }
 </style>
