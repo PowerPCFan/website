88f1e6618d500a7534b20008aa3bfd05d4c36ed1
Charlie <charlies.retro.computing@gmail.com>
2025-08-18 17:11:19 -0400

    add half done local time component made by chatgpt and make some things lowercase because yes

---
+++ src/lib/components/localTime.svelte
@@ -0,0 +1,24 @@
+<script>
+    import { onMount } from 'svelte';
+
+    let time = "";
+
+    function updateTime() {
+        const now = new Date();
+        time = new Intl.DateTimeFormat("en-US", {
+            timeZone: "America/New_York",
+            hour: "2-digit",
+            minute: "2-digit",
+            second: "2-digit",
+            hour12: false
+        }).format(now);
+    }
+
+    onMount(() => {
+        updateTime();
+        const interval = setInterval(updateTime, 1000);
+        return () => clearInterval(interval);
+    });
+</script>
+
+<p>{time}</p>
