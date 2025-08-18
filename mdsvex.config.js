332eac7bf680eeabc5da2f96ec942da9f9fec159
Charlie <charlies.retro.computing@gmail.com>
2025-08-17 20:11:28 -0400

    added rehype autolink headings and made my accordion component more robust

--- mdsvex.config.js
+++ mdsvex.config.js
@@ -1,5 +1,7 @@
 // mdsvex.config.js
 import rehypeExternalLinks from 'rehype-external-links';
+import rehypeSlug from 'rehype-slug';
+import rehypeAutolinkHeadings from 'rehype-autolink-headings'
 import { defineMDSveXConfig as defineConfig } from 'mdsvex';
 
 const config = defineConfig({
@@ -9,6 +11,21 @@ const config = defineConfig({
         [rehypeExternalLinks, {
             target: '_blank',
             rel: ['noopener', 'noreferrer'],
+        }],
+        [rehypeSlug, {}],
+        [rehypeAutolinkHeadings, {
+            behavior: 'append',
+            properties: {
+                class: "rehype-autolink-headings-link",
+                ariaHidden: true,
+                tabIndex: -1,
+            },
+            content: () => [{
+                type: "element",
+                tagName: "span",
+                properties: { className: ["fa-solid", "fa-hashtag"] },
+                children: [],
+            }],
         }],
     ],
 });
