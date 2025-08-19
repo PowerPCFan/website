816e22eb57c1eaaa59374cd0238d1c82b019ca3b
Charlie <charlies.retro.computing@gmail.com>
2025-08-18 20:24:32 -0400

    COMPONENTS

---
+++ src/lib/components/Card/Utilities/StatusText.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+    let { children } = $props();
+</script>
+
+
+<h3 class="status-text">{@render children()}</h3>
+
+
+<style lang="scss">
+    @use '/static/scss/global.scss' as g;
+
+    .status-text {
+        margin: 0;
+        font-size: 0.9rem;
+        font-weight: 600;
+        color: g.$border;
+        text-transform: uppercase;
+        letter-spacing: 0.5px;
+    }
+</style>
