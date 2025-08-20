4f318752ea65e8b13a99aaa9742f9632a09854f2
Charlie <charlies.retro.computing@gmail.com>
2025-08-20 08:48:43 -0400

    use example.com as background url to trick gifypet into giving me transparent background

--- src/lib/components/Widgets/Pet.svelte
+++ src/lib/components/Widgets/Pet.svelte
@@ -5,10 +5,11 @@
 
 <Card>
     <CardContent>
+        <!-- background is set to example.com to trick it into being transparent -->
         <iframe
             title="GifyPet"
             scrolling="no"
-            src="https://gifypet.neocities.org/pet/pet.html?name=Buddy&dob=1755619147&gender=m&element=Air&pet=dog.gif&map=forest.jpg&background=paper.jpg&tablecolor=black&textcolor=black"
+            src="https://gifypet.neocities.org/pet/pet.html?name=Buddy&dob=1755619147&gender=m&element=Air&pet=dog.gif&map=forest.jpg&background=https%3A%2F%2Fexample.com&tablecolor=white&textcolor=white"
             frameborder="0">
         </iframe>
     </CardContent>
