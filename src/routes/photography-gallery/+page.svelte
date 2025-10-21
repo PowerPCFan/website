40bf8698022d932528fc4f13aa9bd0c3d05437f6
Charlie <charlies.retro.computing@gmail.com>
2025-10-21 16:53:07 -0400

    Artificial delay to redirect page

--- src/routes/photography-gallery/+page.svelte
+++ src/routes/photography-gallery/+page.svelte
@@ -1,4 +1,14 @@
 <script lang="ts">
     import { onMount } from "svelte";
-    onMount(() => { window.location.href = '/gallery' });
+
+    function sleep(secs: number): Promise<void> {
+        return new Promise((resolve) => setTimeout(resolve, secs * 1000));
+    }
+
+    // use artificial delay to give the user some time to read the message
+    onMount(() => { sleep(1.25).then(() => { window.location.href = '/gallery' }) });
 </script>
+
+<div style="margin: 0 auto;">
+    <h1 style="text-align: center;">Redirecting to new page...</h1>
+</div>
