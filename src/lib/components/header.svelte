97a2d12ef4fb19dc27854c3c8a3c1ec6b55651a4
Charlie <charlie@powerpcfan.xyz>
2025-12-13 20:46:06 -0500

    fix stuff

--- src/lib/components/header.svelte
+++ src/lib/components/header.svelte
@@ -15,16 +15,16 @@
             </div>
         </div>
         <div class="navbar-item navbar-dropdown">
+            <span class="navbar-dropdown-trigger">Learn <i id="chevron" class="fa-solid fa-circle-chevron-down"></i></span>
+            <div class="navbar-dropdown-menu">
+                <a class="navbar-dropdown-item" href="/learn/python/">Python</a>
+            </div>
+        </div>
+        <div class="navbar-item navbar-dropdown">
             <span class="navbar-dropdown-trigger">Project Docs <i id="chevron" class="fa-solid fa-circle-chevron-down"></i></span>
             <div class="navbar-dropdown-menu">
                 <a class="navbar-dropdown-item" href="/docs/hardwareswap-listing-scraper/">HardwareSwap Listing Scraper</a>
                 <a class="navbar-dropdown-item" href="/docs/kroma/v2">Kroma</a>
-            </div>
-        </div>
-        <div class="navbar-item navbar-dropdown">
-            <span class="navbar-dropdown-trigger">Learn <i id="chevron" class="fa-solid fa-circle-chevron-down"></i></span>
-            <div class="navbar-dropdown-menu">
-                <a class="navbar-dropdown-item" href="/learn/python/">HardwareSwap Listing Scraper</a>
             </div>
         </div>
         <div class="navbar-item navbar-dropdown">
