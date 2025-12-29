a26a884edb2fc1e2b55543342a8541a39a1965bf
Charlie <charlie@powerpcfan.xyz>
2025-12-29 16:59:25 -0500

    add thingy to thingy

---
+++ src/routes/ebay-listing-scraper-discord-pings-internal/+layout.svelte
@@ -0,0 +1,15 @@
+<script>
+    let { children } = $props();
+</script>
+
+<div class="container">
+    {@render children()}
+</div>
+
+<style>
+    .container {
+        padding-inline: 1.5rem;
+        padding-block: 0.25rem;
+        margin: 0;
+    }
+</style>
---
+++ src/routes/ebay-listing-scraper-discord-pings-internal/error-retrieving-seller-url/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+    import { page } from '$app/state';
+    import { onMount } from 'svelte';
+
+    let ebaySellerName: string | null = null;
+
+    onMount(() => {
+        ebaySellerName = page?.url?.searchParams?.get('sellerName') || null;
+    })
+</script>
+
+<h1>Uh-oh! An error occurred.</h1>
+
+<p style="font-size: 1.1rem;">There was an error retrieving the seller URL.</p>
+
+{#if ebaySellerName}
+<p style="font-weight: bold; font-style: italic;">&#128161;Tip&colon; Try searching for &quot;{ebaySellerName}&quot; on eBay.</p>
+{/if}
